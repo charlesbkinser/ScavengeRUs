a899f77c830c8819a2b38e35b074c74254b52abb
Jean Bilong <BILONG@ETSU.EDU>
2023-12-06 14:04:20 -0500

    Resolved merge conflicts... Added batch user creation feature and corresponding UI changes(Note:this is still a working progress)

--- ScavengeRUs/Controllers/UserController.cs
+++ ScavengeRUs/Controllers/UserController.cs
@@ -8,6 +8,12 @@ using System;
 using System.Security.Claims;
 using System.IO;
 using Microsoft.VisualBasic.FileIO;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Drawing.Printing;
+
+
 
 
 
@@ -22,17 +28,22 @@ namespace ScavengeRUs.Controllers
     {
         private readonly IUserRepository _userRepo;
         private readonly Functions _functions;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly ILogger<UserController> _logger;
         string defaultPassword = "Etsupass12!";
 
         /// <summary>
         /// This is the dependecy injection for the User Repository that connects to the database
         /// </summary>
         /// <param name="userRepo"></param>
-        public UserController(IUserRepository userRepo, IConfiguration configuration)
+        public UserController(UserManager<ApplicationUser> userManager, IUserRepository userRepo, IConfiguration configuration, ILogger<UserController> logger)
         {
             _userRepo = userRepo;
             _functions = new Functions(configuration);
-
+            _logger = logger;
+            _userManager = userManager;
         }
         /// <summary>
         /// This is the landing page for www.localhost.com/user/manage aka "Admin Portal"
@@ -43,12 +54,12 @@ namespace ScavengeRUs.Controllers
             var users = await _userRepo.ReadAllAsync(); //Reads all the users in the db
 
             //if the admin didn't search for anything just return all the users
-            if(string.IsNullOrEmpty(searchString))
+            if (string.IsNullOrEmpty(searchString))
                 return View(users);  //Right click and go to view to see HTML
 
             //this line of code filters out all the users whose emails and phone numbers do not
             //contain the search string
-            var searchResults = users.Where(user => user.Email.Contains(searchString) 
+            var searchResults = users.Where(user => user.Email.Contains(searchString)
             || !string.IsNullOrEmpty(user.PhoneNumber) && user.PhoneNumber.Contains(searchString));
 
             return View(searchResults);
@@ -58,7 +69,7 @@ namespace ScavengeRUs.Controllers
         /// </summary>
         /// <param name="username"></param>
         /// <returns></returns>
-        public async Task<IActionResult> Edit([Bind(Prefix = "id")]string username)
+        public async Task<IActionResult> Edit([Bind(Prefix = "id")] string username)
         {
             //await _functions.SendEmail("4239006885@txt.att.net", "Hello, from ASP.NET", "Body");
             //await CreateUsers("testdata.csv");
@@ -85,7 +96,7 @@ namespace ScavengeRUs.Controllers
         /// </summary>
         /// <param name="username"></param>
         /// <returns></returns>
-        public async Task<IActionResult> Delete([Bind(Prefix ="id")]string username)
+        public async Task<IActionResult> Delete([Bind(Prefix = "id")] string username)
         {
             var user = await _userRepo.ReadAsync(username);
             if (user == null)
@@ -101,7 +112,7 @@ namespace ScavengeRUs.Controllers
         /// <returns></returns>
         /// 
         [HttpPost, ActionName("Delete")]
-        public async Task<IActionResult> DeleteConfirmed([Bind(Prefix = "id")]string username)
+        public async Task<IActionResult> DeleteConfirmed([Bind(Prefix = "id")] string username)
         {
             await _userRepo.DeleteAsync(username);
             return RedirectToAction("Manage");
@@ -111,7 +122,7 @@ namespace ScavengeRUs.Controllers
         /// </summary>
         /// <param name="username"></param>
         /// <returns></returns>
-        public async Task<IActionResult> Details([Bind(Prefix = "id")]string username)
+        public async Task<IActionResult> Details([Bind(Prefix = "id")] string username)
         {
             var user = await _userRepo.ReadAsync(username);
 
@@ -141,7 +152,7 @@ namespace ScavengeRUs.Controllers
                 return RedirectToAction("Details", new { id = user.UserName });
             }
             return View(user);
-            
+
         }
         /// <summary>
         /// This is the profile page for a user /user/profile/
@@ -154,15 +165,167 @@ namespace ScavengeRUs.Controllers
             return View(currentUser);
         }
 
-        public async Task<IActionResult> CreateUsers()
+        /// <summary>
+        /// HTTP GET action to display the CreateUsers view.
+        /// </summary>
+        /// <returns>The CreateUsers view.</returns>
+        [HttpGet]
+        public IActionResult CreateUsers()
         {
-            //filePath = "Services/Users.csv";
-            //serverUrl = $"{Request.Scheme}://{Request.Host}";
-            //await _userRepo.CreateUsers(filePath, serverUrl);
-
             return View();
         }
 
-        
+
+        /// <summary>
+        /// HTTP POST action to create users from an uploaded CSV file.
+        /// </summary>
+        /// <param name="uploadedFile">The CSV file containing user data.</param>
+        /// <returns>A JSON result indicating the success or failure of user creation.</returns>
+        [HttpPost]
+        public async Task<IActionResult> CreateUsers(IFormFile uploadedFile)
+        {
+            if (uploadedFile == null || uploadedFile.Length == 0)
+            {
+                return Json(new { success = false, message = "Please upload a CSV file." });
+            }
+
+            var errorList = new List<string>();
+
+            try
+            {
+                using (var stream = new StreamReader(uploadedFile.OpenReadStream()))
+                {
+                    string content = await stream.ReadToEndAsync();
+                    var users = ParseCSVToUsers(content);
+
+                    foreach (var user in users)
+                    {
+                        var result = await _userManager.CreateAsync(user, defaultPassword);
+                        if (!result.Succeeded)
+                        {
+                            errorList.Add($"Error creating user {user.Email}: {string.Join(", ", result.Errors.Select(e => e.Description))}");
+                        }
+                    }
+                }
+
+                if (errorList.Any())
+                {
+                    return Json(new { success = false, message = "Some users could not be created.", errors = errorList });
+                }
+                else
+                {
+                    return Json(new { success = true, message = "All users created successfully." });
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error processing uploaded file.");
+                return Json(new { success = false, message = "An error occurred while processing the file." });
+            }
+        }
+
+        /// <summary>
+        /// Parses a CSV string to create a list of ApplicationUser objects.
+        /// </summary>
+        /// <param name="csvContent">The CSV content as a string.</param>
+        /// <returns>A list of ApplicationUser objects.</returns>
+
+        private List<ApplicationUser> ParseCSVToUsers(string csvContent)
+        {
+            var users = new List<ApplicationUser>();
+            var lines = csvContent.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var line in lines)
+            {
+                var fields = line.Split(',');
+
+                if (fields.Length == 5 && fields.All(field => !string.IsNullOrWhiteSpace(field)))
+                {
+                    var sanitizedEmail = WebUtility.HtmlEncode(fields[3].Trim());
+                    var sanitizedPhone = WebUtility.HtmlEncode(fields[2].Trim());
+
+                    if (!IsValidEmail(sanitizedEmail) || !IsValidPhoneNumber(sanitizedPhone))
+                    {
+                        continue; // Skip invalid entries
+                    }
+
+                    if (!Enum.TryParse<Carriers>(fields[4].Trim(), true, out var carrier))
+                    {
+                        continue; // Skip if carrier parsing fails
+                    }
+
+                    var user = new ApplicationUser
+                    {
+                        FirstName = fields[0].Trim(),
+                        LastName = fields[1].Trim(),
+                        PhoneNumber = sanitizedPhone,
+                        Email = sanitizedEmail,
+                        UserName = sanitizedEmail, // Assuming email as username
+                        Carrier = carrier
+                    };
+
+                    users.Add(user);
+                }
+            }
+
+            return users;
+        }
+
+        /// <summary>
+        /// Validates an email address format.
+        /// </summary>
+        /// <param name="email">The email address to validate.</param>
+        /// <returns>True if the email is valid, false otherwise.</returns>
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Validates a US phone number format.
+        /// </summary>
+        /// <param name="number">The phone number to validate.</param>
+        /// <returns>True if the phone number is valid, false otherwise.</returns>
+        private bool IsValidPhoneNumber(string number)
+        {
+            return Regex.IsMatch(number, @"^\d{10}$"); // US phone number format
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
